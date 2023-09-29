86c511cee75d874abd55cacb22b51ea97984958b
MacReiben <macreiben@gmail.com>
2023-09-29 21:47:04 +0200

    model simplification.

--- src/PitWallDataGatheringApi/PitWallDataGatheringApi/Controllers/TelemetryController.cs
+++ src/PitWallDataGatheringApi/PitWallDataGatheringApi/Controllers/TelemetryController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PitWallDataGatheringApi.Models.Apis;
-using PitWallDataGatheringApi.Models.Prom;
 using Prometheus;
 
 namespace PitWallDataGatheringApi.Controllers
@@ -9,55 +8,43 @@ namespace PitWallDataGatheringApi.Controllers
     [ApiController]
     public class TelemetryController : ControllerBase
     {
-        private readonly Gauge _gauge;
+        private readonly Gauge _gaugeLapTimes;
         private readonly Gauge _gaugeTyre;
-        private readonly Gauge _gaugeFrontLeftTyre;
-        private readonly Gauge _gaugeFrontRightTyre;
-        private readonly Gauge _gaugeRearLeftTyre;
-        private readonly Gauge _gaugeRearRightTyre;
-        readonly string[] labels = new[] { "Pilot1", "Pilot2", "Pilot3", "Pilot4", "Pilot5", "All" };
-        //readonly string[] tyreLabels = new[] { "FrontLeftWear", "FrontRightWear", "RearLeftWear", "RearRightWear" };
         readonly string[] tyreLabels = new[] { "TyreWear" };
+        readonly string[] pilotLabels = new[] { "Pilot" };
 
         public TelemetryController()
         {
-            var config = new GaugeConfiguration();
-            config.LabelNames = new[] { "Pilot1", "All" };
-
             // ---
 
             var configTyres = new GaugeConfiguration();
             configTyres.LabelNames = tyreLabels;
             _gaugeTyre = Metrics.CreateGauge("FuelAssistant_tyres", "Tyres information.", configTyres);
 
+            // ---
 
-            _gauge = Metrics.CreateGauge("FuelAssistant_laptime_milliseconds", "Car laptimes in milliseconds.", config);
-
-
-            _gaugeFrontLeftTyre = Metrics.CreateGauge("FuelAssistant_tyreWear_front_left", "Tyre wear in percent for front left tyre.");
-            _gaugeFrontRightTyre = Metrics.CreateGauge("FuelAssistant_tyreWear_front_right", "Tyre wear in percent for front right tyre.");
-            _gaugeRearLeftTyre = Metrics.CreateGauge("FuelAssistant_tyreWear_rear_left", "Tyre wear in percent from rear left tyre.");
-            _gaugeRearRightTyre = Metrics.CreateGauge("FuelAssistant_tyreWear_rear_right", "Tyre wear in percent from rear riht tyre.");
+            var config = new GaugeConfiguration();
+            config.LabelNames = pilotLabels;
+            _gaugeLapTimes = Metrics.CreateGauge("FuelAssistant_laptimes", "Car laptimes in milliseconds.", config);
         }
 
         [HttpPost]
         public void Post(TelemetryModel telemetry)
         {
-            PromLapData sample = new PromLapData();
+            // ------
+
+            _gaugeTyre.WithLabels("FrontLeft").Set(telemetry.Tyres.FrontLeftWear);
+
+            _gaugeTyre.WithLabels("FrontRight").Set(telemetry.Tyres.FrontRightWear);
+
+            _gaugeTyre.WithLabels("RearLeft").Set(telemetry.Tyres.ReartLeftWear);
+
+            _gaugeTyre.WithLabels("RearRight").Set(telemetry.Tyres.RearRightWear);
 
             // ------
 
-            sample.WithLaptimeMilliseconds(telemetry.LaptimeMilliseconds);
-
-            sample.WithPilotLaptime(telemetry.PilotName, telemetry.LaptimeMilliseconds);
-
-            // ------
-
-            _gauge.Set(sample.LaptimeMilliseconds);
-
-            Gauge.Child temp = _gaugeTyre.WithLabels("FrontLeft");
-
-            temp.Set(0.5);
+            _gaugeLapTimes.WithLabels(telemetry.PilotName).Set(telemetry.LaptimeMilliseconds);
+            _gaugeLapTimes.WithLabels("All").Set(telemetry.LaptimeMilliseconds);
         }
     }
 }
--- src/PitWallDataGatheringApi/PitWallDataGatheringApi/Models/Apis/TelemetryModel.cs
+++ src/PitWallDataGatheringApi/PitWallDataGatheringApi/Models/Apis/TelemetryModel.cs
@@ -2,7 +2,11 @@
 {
     public sealed class TelemetryModel
     {
-        public string PilotName { get;  set; }
+        public TelemetryModel()
+        {
+            Tyres = new Tyres();
+        }
+        public string PilotName { get; set; }
         public int LaptimeMilliseconds { get; set; }
 
         public Tyres Tyres { get; set; }
