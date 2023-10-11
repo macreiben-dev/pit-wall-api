658eecbbe72e5fde982f295057cbb276c9724f9c
MacReiben <macreiben@gmail.com>
2023-10-11 17:26:28 +0200

    healthcheck controller added.

---
+++ src/PitWallDataGatheringApi/PitWallDataGatheringApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PitWallDataGatheringApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
