b221e24adeff55423a88967415d39596830e1fea
MacReiben <macreiben@gmail.com>
2023-10-26 09:35:44 +0200

    introducint avg wetness repository

---
+++ src/PitWallDataGatheringApi/PitWallDataGatheringApi/Repositories/WeatherConditions/AvgWetnessRepository.cs
@@ -0,0 +1,52 @@
+﻿using Prometheus;
+
+namespace PitWallDataGatheringApi.Repositories.WeatherConditions
+{
+    public sealed class AvgWetnessRepository : IDocumentationLaptimeSerie, IAvgWetnessRepository
+    {
+        private const string LocalSerieName = "road_wetness_avg_percent";
+        private readonly Gauge _gauge;
+        readonly string[] _labels = new[] { "Pilot" };
+
+        public string SerieName => LocalSerieName;
+
+        public string[] Labels => _labels;
+
+        public string Description => "Road wetness in percent.";
+
+        public AvgWetnessRepository()
+        {
+            var config = new GaugeConfiguration();
+
+            config.LabelNames = _labels;
+
+            _gauge = Metrics.CreateGauge(
+                SerieName,
+                "Average road wetness in percent.",
+                config);
+        }
+
+        public void Update(double? laptime, string pilotName)
+        {
+            UpdateGauge(
+               laptime,
+               pilotName,
+               _gauge);
+
+            UpdateGauge(
+                laptime,
+                "All",
+                _gauge);
+        }
+
+        private void UpdateGauge(double? data, string gaugeLabel, Gauge gauge)
+        {
+            if (!data.HasValue)
+            {
+                return;
+            }
+
+            gauge.WithLabels(gaugeLabel).Set(data.Value);
+        }
+    }
+}
