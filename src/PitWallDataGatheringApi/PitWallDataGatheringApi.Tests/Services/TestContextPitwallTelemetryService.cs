8429f04a939b9dc5d99c04893b553b3e67afc0b2
MacReiben <macreiben@gmail.com>
2023-10-27 09:23:41 +0200

    Tests reoganization

---
+++ src/PitWallDataGatheringApi/PitWallDataGatheringApi.Tests/Services/TestContextPitwallTelemetryService.cs
@@ -0,0 +1,22 @@
+﻿using PitWallDataGatheringApi.Repositories;
+using PitWallDataGatheringApi.Repositories.Tyres;
+using PitWallDataGatheringApi.Services;
+
+namespace PitWallDataGatheringApi.Tests.Services
+{
+    public class TestContextPitwallTelemetryService
+    {
+        public TestContextPitwallTelemetryService(PitwallTelemetryService target, ITyreWearRepository tyreWearRepository, ITyresTemperaturesRepository tyreTemperature, ILaptimeRepository laptimeRepository)
+        {
+            Target = target;
+            TyreWearRepository = tyreWearRepository;
+            TyreTemperature = tyreTemperature;
+            LaptimeRepository = laptimeRepository;
+        }
+
+        public PitwallTelemetryService Target { get; }
+        public ITyreWearRepository TyreWearRepository { get; }
+        public ITyresTemperaturesRepository TyreTemperature { get; }
+        public ILaptimeRepository LaptimeRepository { get; }
+    }
+}
