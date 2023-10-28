845a1f3a005e2c5111e786bf6c0d6ee6ae067f77
MacReiben <macreiben@gmail.com>
2023-10-28 07:43:13 +0200

    adding the ioc dependency graph test.

---
+++ src/PitWallDataGatheringApi/PitWallDataGatheringApi.Tests/IoCInitializerTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace PitWallDataGatheringApi.Tests
+{
+    public class IoCInitializerTest
+    {
+        [Fact]
+        public void WHEN_iocPart_defined_THEN_doNot_fail()
+        {
+            var builder = WebApplication.CreateBuilder();
+
+            IoCInitializer.Initialize(builder.Services);
+
+            var app = builder.Build();
+
+            var services = builder.Services
+                .Where(c => c.ServiceType.AssemblyQualifiedName.StartsWith("PitWallDataGatheringApi"));
+
+            foreach (var service in services)
+            {
+                var current = app.Services.GetService(service.ServiceType);
+            }
+        }
+    }
+}
---
+++ src/PitWallDataGatheringApi/PitWallDataGatheringApi/IoCInitializer.cs
@@ -0,0 +1,37 @@
+﻿using PitWallDataGatheringApi.Repositories.Tyres;
+using PitWallDataGatheringApi.Repositories;
+using PitWallDataGatheringApi.Services;
+using PitWallDataGatheringApi.Repositories.WeatherConditions;
+
+namespace PitWallDataGatheringApi
+{
+    public class IoCInitializer
+    {
+        public static void Initialize(IServiceCollection services)
+        {
+
+            // Add services to the container.
+
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddSingleton<ITyreWearRepository, TyreWearRepository>();
+            services.AddSingleton<ILaptimeRepository, LaptimeRepository>();
+            services.AddSingleton<ITyresTemperaturesRepository, TyresTemperaturesRepository>();
+            services.AddSingleton<IAvgWetnessRepository, AvgWetnessRepository>();
+            services.AddSingleton<IAirTemperatureRepository, AirTemperatureRepository>();
+
+            services.AddScoped<IDocumentationLaptimeSerie, LaptimeRepository>();
+            services.AddScoped<IDocumentationTyresWearSerie, TyreWearRepository>();
+            services.AddScoped<IDocumentationTyresTemperaturesSerie, TyresTemperaturesRepository>();
+            
+
+            services.AddSingleton<IPitwallTelemetryService, PitwallTelemetryService>();
+            services.AddSingleton<ITelemetryModelMapper, TelemetryModelMapper>();
+
+            services.AddSingleton<ISimerKeyRepository, SimerKeyRepository>();
+        }
+    }
+}
