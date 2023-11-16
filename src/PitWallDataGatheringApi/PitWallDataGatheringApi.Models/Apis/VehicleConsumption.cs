87fe735879ca479d110872c576e5f4ebcae7c540
MacReiben <macreiben@gmail.com>
2023-11-16 10:40:33 +0100

    add consumption model to contract.

---
+++ src/PitWallDataGatheringApi/PitWallDataGatheringApi.Models/Apis/VehicleConsumption.cs
@@ -0,0 +1,17 @@
+﻿namespace PitWallDataGatheringApi.Models.Apis
+{
+    public class VehicleConsumption
+    {
+        public double? Fuel { get; set; }
+
+        public double? MaxFuel { get; set; }
+
+        public double? LastLapConsumption { get; set; }
+
+        public double? ComputedLiterPerLaps { get; set; }
+
+        public double? ComputedRemainingLaps { get; set; }
+
+        public double? ComputedRemainingTime { get; set; }
+    }
+}
