b1376e2f83dcd68ce362706aa18e19591e9f189e
MacReiben <macreiben@gmail.com>
2023-12-27 10:42:48 +0100

    added some documentation on gauge factory.

--- src/PitWallDataGatheringApi/PitWallDataGatheringApi.Repositories/Prom/IGaugeWrapperFactory.cs
+++ src/PitWallDataGatheringApi/PitWallDataGatheringApi.Repositories/Prom/IGaugeWrapperFactory.cs
@@ -2,11 +2,22 @@
 {
     public interface IGaugeFactory
     {
+        /// <summary>
+        /// Creates or retrieve existing serie from the serie name.
+        /// </summary>
+        /// <param name="serieName">The name of the serie.</param>
+        /// <param name="description">The description for the documentation entry point.</param>
+        /// <param name="labels">The labels to add to the gauge.</param>
+        /// <returns></returns>
         IGauge Create(
             string serieName,
             string description,
             IEnumerable<string> labels);
 
+        /// <summary>
+        /// List all created series.
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<ISerieDocumentation> ListCreated();
     }
 }
